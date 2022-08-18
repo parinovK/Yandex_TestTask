a878b3b5b52efb7496847912dd7fa635d95244a1
parinovK <62334658+parinovK@users.noreply.github.com>
2022-08-18 03:13:57 +0300

    Update Yandex.cpp
    // Locale: EN
    [1]The structure was replaced by map.
    [2]The variable responsible for the volume of the dictionary was also removed. 
    [3]Function prototypes were written, and the functions themselves are hidden under "int main()"
    // Locale: RU
    [1] Структура была заменена на map. Реализация проще, также занимает меньше места.
    [2] Убрана переменная отвечающая за вес алфавита.
    [3] Описаны прототипы функций, а сами функции спрятаны под "int main()"

--- Yandex.cpp
+++ Yandex.cpp
@@ -1,69 +1,26 @@
-﻿// Yandex.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// Yandex_contest.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int size_Alphabit = 27;
-
-struct Letter {
-	char letter;
-	int stepen;
-};
-
+map <int, char> Letter;
 vector<long long int> digits;
-Letter* let = new Letter[size_Alphabit];
 
-void completion_letter() {
-	Letter litera;
-	int iterator = 0;
-
-	for (char i = 'a'; i <= 'z'; i++)
-	{
-		litera.letter = i;
-		litera.stepen = iterator;
-		let[iterator] = litera;
-		iterator++;
-	}
-	
-	litera.letter = ' ';
-	litera.stepen = iterator;
-	let[iterator] = litera;
-}
-
-void parser(vector<string> buffers, int& iterator)
-{
-	string input_string = buffers.at(1);
-	string temp;
-	
-	if (input_string[iterator] == ' ') iterator++;
-	while (iterator < input_string.length() && input_string[iterator] != ' ')
-	{
-		temp += input_string[iterator];
-		iterator++;
-	}
-	digits.push_back(atoi(temp.c_str()));
-	temp.clear();
-}
-
-char sopostavlenie(int iterator)
-{
-	for (int i = 0; i < size_Alphabit; i++)
-	{
-		if (let[i].stepen == iterator) return let[i].letter;
-	}
-}
+void completion_letter();
+void parser(vector<string> buffers, int& iterator);
+char sopostavlenie(int iterator);
 
 int main()
 {
 	ifstream input("input.txt");
 	string buff;
 	vector<string> buffers;
-	
+
 	while (!input.eof())
 	{
 		getline(input, buff);
@@ -92,19 +49,48 @@ int main()
 				iterator++;
 				temp /= 2;
 			}
-			buff += sopostavlenie(iterator);
-			iterator = 0;
 		}
-		else {	
+		else
 			while (digits.at(i) / 2 != 0)
 			{
 				iterator++;
 				digits.at(i) /= 2;
 			}
-			buff += sopostavlenie(iterator);
-			iterator = 0;
-		}
+		buff += sopostavlenie(iterator);
+		iterator = 0;
 	}
 	cout << buff;
 	return 0;
+}
+
+void completion_letter() {
+	int iterator = 0;
+
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		Letter[iterator] = i;
+		iterator++;
+	}
+	Letter[iterator] = ' ';
+}
+
+void parser(vector<string> buffers, int& iterator)
+{
+	string input_string = buffers.at(1);
+	string temp;
+
+	if (input_string[iterator] == ' ') iterator++;
+	while (iterator < input_string.length() && input_string[iterator] != ' ')
+	{
+		temp += input_string[iterator];
+		iterator++;
+	}
+	digits.push_back(atoi(temp.c_str()));
+	temp.clear();
+}
+
+char sopostavlenie(int iterator)
+{
+	for (const auto& kv : Letter)
+		if (kv.first == iterator) return kv.second;
 }
